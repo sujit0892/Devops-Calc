f94c049baa0c0bbe179f83a39ace4a9ea4aa51f8
sujit0892 <sujitkumar0892@gmail.com>
2021-03-09 11:32:47 +0530

    power func

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public static void main(String[] args){
+        System.out.println("5! = " + fact(5));
+        System.out.println("3 ^ 4 = " + power(3,4));
+    }
+
+    public static int fact(int n){
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+    public static double power(int a,int b){
+        return java.lang.Math.pow(a,b);
+    }
+}
