783a7bae4873d2f89028a3c277e9d91558edd13f
sujit0892 <sujitkumar0892@gmail.com>
2021-03-11 18:48:18 +0530

    Calc code done

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,28 +1,74 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args){
-        System.out.println("5! = " + fact(5));
-        System.out.println("3 ^ 4 = " + power(3,4));
+        Scanner sc = new Scanner(System.in);
+        int ch;
+        long a, b;
+        while(true)
+            {
+                try {
+                    System.out.print("Menu\n1. Factorial\n2. Power\n3. Log\n4. Sqrt\n5. Exit\nEnter choice: ");
+                    ch = sc.nextInt();
+                    switch (ch)
+                    {
+                        case 1: System.out.print("Enter a number: ");
+                                a = sc.nextLong();
+                                System.out.println(fact(a));
+                                break;
+                        case 2: System.out.print("Enter two number a^b : ");
+                            a = sc.nextLong();
+                            b = sc.nextLong();
+                            System.out.println((long)power(a,b));
+                            break;
+                        case 3: System.out.print("Enter a number: ");
+                            a = sc.nextLong();
+                            System.out.println(log(a)+"\n");
+                            break;
+                        case 4: System.out.print("Enter a number: ");
+                            a = sc.nextLong();
+                            System.out.println(sqrt(a)+"\n");
+                            break;
+                        case 5: System.exit(0);
+                            break;
+
+                        default: System.out.println("Wrong Input");
+                    }
+                }
+                catch (InputMismatchException e)
+                {
+                    System.out.println("Wrong Input");
+
+                }
+
+            }
+
+
+
     }
 
-    public static int fact(int n){
-        int fact=1;
-        for(int i=1;i<=n;i++){
+    public static long fact(long n){
+        long fact=1;
+        for(long i=1;i<=n;i++){
             fact=fact*i;
         }
         return fact;
     }
 
-    public static double power(int a,int b){
+    public static double power(long a,long b){
         return java.lang.Math.pow(a,b);
     }
 
-    public static double Log(int a){
+    public static double log(long a){
         return java.lang.Math.log(a);
     }
 
-    public static double sqrt(int a){
+    public static double sqrt(long a){
         return java.lang.Math.sqrt(a);
     }
+
+
 
 
 }
